28d469d1752489eccb0d15da39741a3169b3a39c
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2023-11-15 20:41:06 -0500

    added Wheel Speed Difference functions

---
+++ Functions/WheelSpeedDiff.m
@@ -0,0 +1,15 @@
+function [RearSpeedDiff, FrontSpeedDiff, YRTW] = WheelSpeedDiff(run, car)
+%WHEELSPEEDDIFF Finds actual and hypothetical wheel speed differences
+%   YRTW is the Yaw Rate * Track Width which is the hypothetical optimal
+%   wheel speed difference. 
+
+YawRate = run.GyroYawVelocityIMU*pi/180; % [rad/s]
+TW = car.trackwidthRear; % [m]
+YRTW = abs(YawRate*TW*3.6); %[km/h] + = right, - = left
+RearSpeedL = run.DriveSpeedLeft; 
+RearSpeedR = run.DriveSpeedRight;
+RearSpeedDiff = RearSpeedL-RearSpeedR; 
+FrontSpeedDiff = run.GroundSpeedLeft-run.GroundSpeedRight;
+
+end
+
