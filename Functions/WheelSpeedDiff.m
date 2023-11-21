dacc5d334203856e55a14418fa47a6378f7f7d35
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2023-11-20 20:54:27 -0500

    Added ShockSpeedHistogram function
    Also edited the wheel speed diff functions

--- Functions/WheelSpeedDiff.m
+++ Functions/WheelSpeedDiff.m
@@ -1,11 +1,11 @@
 function [RearSpeedDiff, FrontSpeedDiff, YRTW] = WheelSpeedDiff(run, car)
 %WHEELSPEEDDIFF Finds actual and hypothetical wheel speed differences
 %   YRTW is the Yaw Rate * Track Width which is the hypothetical optimal
-%   wheel speed difference. 
+%   wheel speed difference for the rear axle. 
 
 YawRate = run.GyroYawVelocityIMU*pi/180; % [rad/s]
 TW = car.trackwidthRear; % [m]
-YRTW = abs(YawRate*TW*3.6); %[km/h] + = right, - = left
+YRTW = YawRate*TW*3.6; %[km/h] + = right, - = left
 RearSpeedL = run.DriveSpeedLeft; 
 RearSpeedR = run.DriveSpeedRight;
 RearSpeedDiff = RearSpeedL-RearSpeedR; 
