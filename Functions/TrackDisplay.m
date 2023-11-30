b2b6927202b04d74b38076a36fb7e4af3c813465
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2023-11-29 21:29:08 -0500

    updated the TrackDisplay function
    Added more documentation comments and made ColorLimits optional

--- Functions/TrackDisplay.m
+++ Functions/TrackDisplay.m
@@ -1,15 +1,17 @@
 function [] = TrackDisplay(long, lat, data, colorscheme, colorLimits)
 %TrackDisplay Visualize the track shape, and overlay variables on track
-%   Data is optional - if given it will apply a gradient to the track based
+%   long, lat - GPS Coordinates for the longitude and latitude of the car  
+%   data - optional, if given it will apply a gradient to the track based
 %   on the value of data at that point on track
-%   Colorscheme is optional - input will be passed into colormap()
+%   colorscheme - optional, input will be passed into colormap()
+%   colorLimits - optional, sets the colorbar limits
 
 arguments
     long (1,:)
     lat (1,:)
     data = zeros(size(lat))'
     colorscheme = turbo
-    colorLimits = [0 110]
+    colorLimits = 0; % [0 110] are good speed limits
 end
 
 % Code adapted from 
@@ -17,13 +19,15 @@ end
 x = long;
 y = lat;
 z = data'; % invert with ' to make it one row instead of col
-col = data';  % This is the color, vary with data in this case. inverted
-surface([x;x],[y;y],[z;z],[col;col],...
+color = data';  % This is the color, vary with data in this case. inverted
+surface([x;x],[y;y],[z;z],[color;color],...
         'facecol','no',...
         'edgecol','interp',...
         'linew',1);
 colorbar;
-clim(colorLimits)
+if colorLimits ~= 0
+    clim(colorLimits)
+end
 colormap(colorscheme);
 
 % Unfortunately, there is no color gradient workaround for geoplots (that 
