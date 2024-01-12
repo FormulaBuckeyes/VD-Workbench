91912a57a1ad7d1073a2ae702ecf54382568b609
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2024-01-11 23:14:03 -0500

    Update EventSummaryTemplate.m

--- Event Scripts/EventSummaryTemplate.m
+++ Event Scripts/EventSummaryTemplate.m
@@ -6,6 +6,10 @@ metadata = motecMetadata(filename);
 totalDistance = max(data.Distance);
 totalTime = max(data.Time);
 
+% Replace Event with actual event in real code
+% Replace Subteam with actual subteam in real code
+lapData = parseEventRuns(data); % Need to make this a real thing
+lapTimes = eventLapTimes(lapData); % Need to make this a real thing
 
 %% Summary
 
@@ -16,19 +20,28 @@ fprintf("Run Time: %s", metadata.logTime)
 fprintf("Comments: %s", metadata.comment)
 fprintf("Report Generated: %s", datetime("now"))
 
-% left vs right
 % lap times
 
+% left vs right (average of gLat for all lapData - should be 0)
+% (could be moved to suspension)
 
 %% Subteam
 
-% new plot
+subteamEventPlot(lapData)
+
+% This function should make plot(s)
+% One function per subteam would make formatting very nice
+% There can't be any comments in this script for formatting reasons
+% so do all of it inside the function.
 
 %% Subteam
 
 % new plot
 
 %%
-% outputFilename = "./Logs/Event[Runtime].pdf";
+% This has not been tested yet but should make a pdf, then 
+% rename and move it
+% Need a better naming convention for the file
+% outputFilename = sprintf("./Logs/Event%s.pdf","1");
 % publish("EventSummaryTemplate.m","format","pdf", showCode=false)
-% movefile "EventSummaryTemplate.pdf" "Logs"
+% movefile "EventSummaryTemplate.pdf" outputFilename
