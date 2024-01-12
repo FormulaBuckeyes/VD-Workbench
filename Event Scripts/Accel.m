79347cf9b4f19d490585d4eb6f3917baa4c3d5e8
cmullan27 <132717373+cmullan27@users.noreply.github.com>
2024-01-11 20:54:43 -0500

    Add files via upload

--- Event Scripts/Accel.m
+++ Event Scripts/Accel.m
@@ -42,7 +42,6 @@ for k=1:N
         LaunchName = strcat(t,'_',string(time),'.mat');
         save(LaunchName,'run')
     end
-
 end
 
 
@@ -61,27 +60,27 @@ function [] = AccelDisp(data,t)
     z.Motion = 'Horizontal';
 
     ax1 = nexttile;
-    plot(time,data.GPSSpeed); grid on; hold on;
-    plot(time,data.DriveSpeed)
+    plot(time,data.GPSSpeed,'b-'); grid on; hold on;
+    plot(time,data.DriveSpeed,'r-')
     setAxesZoomConstraint(z,ax1,'x');
     ylabel('Speed [mph]'); ylim([0 70]);
     legend('GPS Speed','Drive Speed','Location','southeast');
 
     ax2 = nexttile;
     slip = ((data.DriveSpeed)./data.GPSSpeed-1);
-    plot(time,slip); grid on;
+    plot(time,slip,'b-'); grid on;
     setAxesZoomConstraint(z,ax2,'x');
     ylabel('Slip'); ylim([0 max(slip)+5]);
     
     ax3 = nexttile;
-    plot(time,data.EngineRPM); grid on; hold on;
-    plot(time,data.LaunchAimRPM);
+    plot(time,data.EngineRPM,'b-'); grid on; hold on;
+    plot(time,data.LaunchAimRPM,'r-');
     setAxesZoomConstraint(z,ax3,'x');
     ylabel('Engine Speed [RPM]'); ylim([4000 12500]);
     legend('RPM','Launch RPM Target','Location','southeast')
 
     ax4 = nexttile; hold on;
-    plot(time,gLongf);
+    plot(time,gLongf,'b-');
     plot(time,gLong,'r.','MarkerSize',1); 
     grid on;
     setAxesZoomConstraint(z,ax4,'x');
@@ -90,32 +89,33 @@ function [] = AccelDisp(data,t)
 
     linkaxes([ax1 ax2 ax3 ax4],'x');
     xlim([min(time) max(time)]);
-    
+    % Suspension Plotting 1
     [heave,pitch] = SusCalc(data,n);
     figure;
     tiledlayout(3,1); sgtitle(sprintf('Sus Heave and Pitch - Time: %.3f [s]', t));
     z = zoom;
     z.Motion = 'Horizontal';
     ax1 = nexttile;
-    plot(time,data.GPSSpeed); grid on; hold on;
-    plot(time,data.DriveSpeed)
+    plot(time,data.GPSSpeed,'b-'); grid on; hold on;
+    plot(time,data.DriveSpeed,'r-')
     setAxesZoomConstraint(z,ax1,'x');
     ylabel('Speed [mph]'); ylim([0 70]);
     legend('GPS Speed','Drive Speed','Location','southeast');
 
     ax2 = nexttile; grid on; hold on;
-    plot(time,heave); 
+    plot(time,heave,'b-'); 
     ylabel('Heave [mm]'); ylim([-25 1])
     setAxesZoomConstraint(z,ax2,'x');
 
     ax3 = nexttile; grid on; hold on;
-    plot(time,pitch);
-    ylabel('Pitch [mm]'); ylim([-1 30]);
+    plot(time,pitch,'b-');
+    ylabel('Pitch [deg]'); ylim([-0.15 0.25]);
     setAxesZoomConstraint(z,ax3,'x');
 
     linkaxes([ax1 ax2 ax3 ax4],'x');
     xlim([min(time) max(time)]);
-
+    % Suspension Plotting 2
+    SusDisp(data,n,t);
 end
 
 function [heave,pitch] = SusCalc(data,smoothFactor)
@@ -127,7 +127,55 @@ function [heave,pitch] = SusCalc(data,smoothFactor)
     SP_RF = smoothdata(SuspPosFR,'gaussian',smoothFactor);
     SP_LR = smoothdata(SuspPosRL,'gaussian',smoothFactor);
     SP_RR = smoothdata(SuspPosRR,'gaussian',smoothFactor);
-
+    MRF = 0.955; MRR = 0.99; L = 1616; % Motion Ratios [~] + Wheelbase [mm]
     heave = SP_LF+SP_RF+SP_LR+SP_RR;
-    pitch = SP_LF+SP_RF-SP_LR-SP_RR;
+    pitch = atand(((SP_LF+SP_RF)*MRF/2-(SP_LR-SP_RR)*MRR/2)/L);
+end
+
+function SusDisp(data,smoothFactor,t)
+    time = data.Time;
+    SuspPosFL = data.SuspPosFL-data.SuspPosFL(1);
+    SuspPosFR = data.SuspPosFR-data.SuspPosFR(1);
+    SuspPosRL = data.SuspPosRL-data.SuspPosRL(1);
+    SuspPosRR = data.SuspPosRR-data.SuspPosRR(1);
+    SP_LF = smoothdata(SuspPosFL,'gaussian',smoothFactor);
+    SP_RF = smoothdata(SuspPosFR,'gaussian',smoothFactor);
+    SP_LR = smoothdata(SuspPosRL,'gaussian',smoothFactor);
+    SP_RR = smoothdata(SuspPosRR,'gaussian',smoothFactor);
+    
+    figure;
+    tiledlayout(2,2); sgtitle(sprintf('Damper Positions - Time: %.3f [s]', t));
+    z = zoom;
+    z.Motion = 'Horizontal';
+
+    ax1 = nexttile; 
+    plot(time,SuspPosFL,'r.','MarkerSize',3); grid on; hold on;
+    plot(time,SP_LF,'b-');
+    setAxesZoomConstraint(z,ax1,'x'); 
+    ylabel('Damper Position [mm]'); xlabel('Time [s]'); ylim([-3 8]);
+    legend('Raw','Filtered','Location','Best'); title('Front Left');
+
+    ax2 = nexttile; 
+    plot(time,SuspPosFR,'r.','MarkerSize',3); grid on; hold on;
+    plot(time,SP_RF,'b-');
+    setAxesZoomConstraint(z,ax2,'x');
+    ylabel('Damper Position [mm]'); xlabel('Time [s]'); ylim([-3 8]);
+    legend('Raw','Filtered','Location','Best'); title('Front Right');
+
+    ax3 = nexttile; 
+    plot(time,SuspPosRL,'r.','MarkerSize',3); grid on; hold on;
+    plot(time,SP_LR,'b-');
+    setAxesZoomConstraint(z,ax3,'x');
+    ylabel('Damper Position [mm]'); xlabel('Time [s]'); ylim([-7 1]);
+    legend('Raw','Filtered','Location','Best'); title('Rear Left');
+
+    ax4 = nexttile; 
+    plot(time,SuspPosRR,'r.','MarkerSize',3); grid on; hold on;
+    plot(time,SP_RR,'b-');
+    setAxesZoomConstraint(z,ax4,'x');
+    ylabel('Damper Position [mm]'); xlabel('Time [s]'); ylim([-7 1]);
+    legend('Raw','Filtered','Location','Best'); title('Rear Right');
+
+    linkaxes([ax1 ax2 ax3 ax4],'x');
+    xlim([min(time) max(time)]);
 end
